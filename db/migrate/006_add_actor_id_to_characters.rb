489c50f613a32c74cd0f96acc2669f7ea90223e1
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-15 20:20:34 +0000

    automatically backed up by learn

--- db/migrate/006_add_actor_id_to_characters.rb
+++ db/migrate/006_add_actor_id_to_characters.rb
@@ -1,5 +1,5 @@
 class AddCatchphraseToCharacter < ActiveRecord::Migration[4.2]
   def change
-    add_column :character, :catchphrase, :string
+    add_column :characters, :actor_id, :integer
   end
 end
