117a6c6b0135b16d6f91cb758b2c81ee1dd08ad5
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-15 20:28:41 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,7 +3,7 @@ class Character < ActiveRecord::Base
   belongs_to :show
 
   def say_that_thing_you_say
-    "#{self.actor.name} always says: #{self.catchphrase}"
+    "#{self.actor} always says: #{self.catchphrase}"
   end
 
 end
